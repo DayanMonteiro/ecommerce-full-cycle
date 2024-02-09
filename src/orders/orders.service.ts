049c2661a97315387bb607dd1f65ae7b140d667f
DayanMonteiro <dmsmamb@gmail.com>
2024-02-09 18:27:05 -0300

    create costumer for rabbitmq

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,17 +1,87 @@
-import { Injectable } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
-import { Order } from './entities/order.entity';
-import { Product } from '../products/entities/product.entity';
-import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+// import { Injectable } from '@nestjs/common';
+// import { CreateOrderDto } from './dto/create-order.dto';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { In, Repository } from 'typeorm';
+// import { Order } from './entities/order.entity';
+// import { Product } from '../products/entities/product.entity';
+// import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+
+// @Injectable()
+// export class OrdersService {
+//   constructor(
+//     @InjectRepository(Order) private orderRepo: Repository<Order>,
+//     @InjectRepository(Product) private productRepo: Repository<Product>,
+//     private amqpConnection: AmqpConnection,
+//   ) {}
+
+//   async create(createOrderDto: CreateOrderDto & { client_id: number }) {
+//     const productIds = createOrderDto.items.map((item) => item.product_id);
+//     const uniqueProductIds = [...new Set(productIds)];
+//     const products = await this.productRepo.findBy({
+//       id: In(uniqueProductIds),
+//     });
+
+//     if (products.length !== uniqueProductIds.length) {
+//       throw new Error(
+//         `Algum produto não existe. Produtos passados ${productIds}, produtos encontrados ${products.map((product) => product.id)}`,
+//       );
+//     }
+
+//     const order = Order.create({
+//       client_id: createOrderDto.client_id,
+//       items: createOrderDto.items.map((item) => {
+//         const product = products.find(
+//           (product) => product.id === item.product_id,
+//         );
+//         return {
+//           price: product.price,
+//           product_id: item.product_id,
+//           quantity: item.quantity,
+//         };
+//       }),
+//     });
+//     await this.orderRepo.save(order);
+//     await this.amqpConnection.publish('amq.direct', 'OrderCreated', {
+//       order_id: order.id,
+//       card_hash: createOrderDto.card_hash,
+//       total: order.total,
+//     });
+//     return order;
+//   }
+
+//   findAll(client_id: number) {
+//     return this.orderRepo.find({
+//       where: {
+//         client_id,
+//       },
+//       order: {
+//         created_at: 'DESC',
+//       },
+//     });
+//   }
+
+//   findOne(id: string, client_id: number) {
+//     return this.orderRepo.findOneByOrFail({
+//       id,
+//       client_id,
+//     });
+//   }
+// }
+
+import { Injectable } from "@nestjs/common";
+import { CreateOrderDto } from "./dto/create-order.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Order } from "./entities/order.entity";
+import { In, Repository } from "typeorm";
+import { Product } from "../products/entities/product.entity";
+import { AmqpConnection } from "@golevelup/nestjs-rabbitmq";
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order) private orderRepo: Repository<Order>,
     @InjectRepository(Product) private productRepo: Repository<Product>,
-    private amqpConnection: AmqpConnection,
+    private amqpConnection: AmqpConnection
   ) {}
 
   async create(createOrderDto: CreateOrderDto & { client_id: number }) {
@@ -23,7 +93,7 @@ export class OrdersService {
 
     if (products.length !== uniqueProductIds.length) {
       throw new Error(
-        `Algum produto não existe. Produtos passados ${productIds}, produtos encontrados ${products.map((product) => product.id)}`,
+        `Algum produto não existe. Produtos passados ${productIds}, produtos encontrados ${products.map((product) => product.id)}`
       );
     }
 
@@ -31,7 +101,7 @@ export class OrdersService {
       client_id: createOrderDto.client_id,
       items: createOrderDto.items.map((item) => {
         const product = products.find(
-          (product) => product.id === item.product_id,
+          (product) => product.id === item.product_id
         );
         return {
           price: product.price,
@@ -41,11 +111,12 @@ export class OrdersService {
       }),
     });
     await this.orderRepo.save(order);
-    await this.amqpConnection.publish('amq.direct', 'OrderCreated', {
+    await this.amqpConnection.publish("amq.direct", "OrderCreated", {
       order_id: order.id,
       card_hash: createOrderDto.card_hash,
       total: order.total,
     });
+    //publish diretamente numa fila
     return order;
   }
 
@@ -55,7 +126,7 @@ export class OrdersService {
         client_id,
       },
       order: {
-        created_at: 'DESC',
+        created_at: "DESC",
       },
     });
   }
@@ -65,5 +136,29 @@ export class OrdersService {
       id,
       client_id,
     });
+  }
+
+  async pay(id: string) {
+    const order = await this.orderRepo.findOneByOrFail({
+      id,
+    });
+
+    order.pay();
+
+    await this.orderRepo.save(order);
+
+    return order;
+  }
+
+  async fail(id: string) {
+    const order = await this.orderRepo.findOneByOrFail({
+      id,
+    });
+
+    order.fail();
+
+    await this.orderRepo.save(order);
+
+    return order;
   }
 }
